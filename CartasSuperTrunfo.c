ef666010164008137bdc7c18d9375a7e7985c2d4
JeremyNpc <guilhermes3684@gmail.com>
2025-02-18 16:09:20 +0000

    projeto_finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,13 +3,22 @@ int main() {
 char R;
 char Codigoc1[20] ="R01";
 char Cidade1[20] ="Rio_de_janeiro";
-int Populaçao1 = 674800; 
+int populacao =674800; 
 float Area1 =1200.25;
 float Pib1 =300.50;
 int Turismo =30;
 
-printf("Estado: %c\n, Codigo_da_carta: %s\n, Cidade: %s\n Populaçao: %d\n Area: %f\n, Pib: %f\n, Pontos_Turisticos: %d ", R, Codigoc1, Cidade1, Populaçao1, Area1, Pib1, Turismo);
+printf("Estado: %c\n, Codigo_da_carta: %s\n, Cidade: %s\n Populaçao: %d\n ,Area: %f\n, Pib: %f\n, Pontos_Turisticos: %d\n ", R, Codigoc1, Cidade1, populacao, Area1, Pib1, Turismo);
 
+char S;
+char codigoc[20] ="SP01";
+char cidade2[20] ="Sao_Paulo";
+int Populacao =12325000;
+float Area2 =1521.11;
+float pib =699.28;
+int Pt = 30;
+
+printf("Estado: %c\n, Codigo_da_carta: %s\n, Cidade: %s\n, Populaçao: %d\n, Area: %f\n, Pib: %f\n, Pontos_Turisticos: %d ", S, codigoc, cidade2, populacao, Area2, pib, Pt);
 
 
 
