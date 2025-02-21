e855f1b0791c549d42964d9dd6a1c086a46173b9
JeremyNpc <guilhermes3684@gmail.com>
2025-02-21 13:00:30 +0000

    terminado desafio intermediario

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,10 +5,14 @@ char Codigoc1[20] ="R01";
 char Cidade1[20] ="Rio_de_janeiro";
 int populacao =674800; 
 float Area1 =1200.25;
-float Pib1 =300.50;
+float Pib1 =3000.50;
 int Turismo =30;
+float Pibpc1 = Pib1 / populacao;
+float Dp1 =  populacao / Area1;
 
-printf("Estado: %c\n, Codigo_da_carta: %s\n, Cidade: %s\n Populaçao: %d\n ,Area: %f\n, Pib: %f\n, Pontos_Turisticos: %d\n ", R, Codigoc1, Cidade1, populacao, Area1, Pib1, Turismo);
+printf("Estado: %c\n, Codigo_da_carta: %s\n, Cidade: %s\n Populaçao: %d habitantes\n ,Area: %f km\n, Pib: %f\n, Pontos_Turisticos: %d\n ", R, Codigoc1, Cidade1, populacao, Area1, Pib1, Turismo);
+printf("Densidade Populacional: %f\n", Dp1);
+printf("Pib per capita: %f\n", Pibpc1);
 
 char S;
 char codigoc[20] ="SP01";
@@ -17,9 +21,12 @@ int Populacao =12325000;
 float Area2 =1521.11;
 float pib =699.28;
 int Pt = 30;
+float Pibpc2 =pib / Populacao;
+float Dp2 = Populacao / Area2;
 
-printf("Estado: %c\n, Codigo_da_carta: %s\n, Cidade: %s\n, Populaçao: %d\n, Area: %f\n, Pib: %f\n, Pontos_Turisticos: %d ", S, codigoc, cidade2, populacao, Area2, pib, Pt);
-
+printf("Estado: %c\n, Codigo_da_carta: %s\n, Cidade: %s\n, Populaçao: %d habitantes\n, Area: %f km\n, Pib: %f\n, Pontos_Turisticos: %d ", S, codigoc, cidade2, populacao, Area2, pib, Pt);
+printf("Densidade Populacional: %f\n", Pibpc2);
+printf("Pib per capita: %f\n", Dp2);
 
 
 }
